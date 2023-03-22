031f3efb8da9c3fd5e484c36fd7524c6899afc3c
teinejo <80319351+teinejo@users.noreply.github.com>
2023-03-23 00:14:59 +0600

    healthcheck endpoint added

---
+++ server/base/views/healthcheck.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from rest_framework import status
+
+def healthcheck(request):
+    try:
+        if request:
+            return HttpResponse(status=status.HTTP_200_OK)
+    except:
+        return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
