00799647b6725380ddff13bbf23a034340e75187
midas-png <y4nlvn@gmail.com>
2023-03-25 17:37:57 +0300

    snapshot tests added home page

--- client/src/pages/__tests__/Home.test.tsx
+++ client/src/pages/__tests__/Home.test.tsx
@@ -2,17 +2,34 @@ import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'features';
 import { Home } from 'pages/Home';
 
-test('Home page render', () => {
-  renderWithTheme(<Home />);
-  const title1 = screen.getByText(/^Land a job$/i);
-  const title2 = screen.getByText(/^the easy way$/i);
-  const subtitle = screen.getByText(
-    /The best place to discover & apply to the coolest start up jobs, without the black box./i
-  );
-  const button = screen.getByText(/^explore$/i);
+describe('Home page render', () => {
+  test('Home page render', () => {
+    renderWithTheme(<Home />);
+    const title1 = screen.getByText(/^Land a job$/i);
+    const title2 = screen.getByText(/^the easy way$/i);
+    const subtitle = screen.getByText(
+      /The best place to discover & apply to the coolest start up jobs, without the black box./i
+    );
+    const button = screen.getByText(/^explore$/i);
 
-  expect(title1).toBeInTheDocument;
-  expect(title2).toBeInTheDocument;
-  expect(subtitle).toBeInTheDocument;
-  expect(button).toBeInTheDocument;
+    expect(title1).toBeInTheDocument;
+    expect(title2).toBeInTheDocument;
+    expect(subtitle).toBeInTheDocument;
+    expect(button).toBeInTheDocument;
+  });
+
+  test('Home page snapshot tests', () => {
+    renderWithTheme(<Home />);
+    const title1 = screen.getByText(/^Land a job$/i);
+    const title2 = screen.getByText(/^the easy way$/i);
+    const subtitle = screen.getByText(
+      /The best place to discover & apply to the coolest start up jobs, without the black box./i
+    );
+    const button = screen.getByText(/^explore$/i);
+
+    expect(title1).toMatchSnapshot();
+    expect(title2).toMatchSnapshot();
+    expect(subtitle).toMatchSnapshot();
+    expect(button).toMatchSnapshot();
+  });
 });
